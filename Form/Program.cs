dbb10f5c6426d104b322da5342edb5fe9c46b389
v-kolp <v.kolpashchikov@gmail.com>
2021-01-08 23:22:33 +0300

    modified:   Form/Program.cs

--- Form/Program.cs
+++ Form/Program.cs
@@ -11,10 +11,11 @@ namespace Form
             bool HavePet = false;
             double MyShoeSize = 40.5;
 
-            Console.WriteLine("My name is " + MyName);
-            Console.WriteLine("My age is " + MyAge);
-            Console.WriteLine("Do I have a pet? " + HavePet);
-            Console.WriteLine("My shoe size is " + MyShoeSize);
+            Console.WriteLine($"My name is {MyName}");
+            Console.WriteLine($"My age is {MyAge}");
+            Console.WriteLine($"Do I have a pet? {HavePet}");
+            Console.WriteLine($"My shoe size is {MyShoeSize}");
+
             Console.WriteLine("\nДля продолжения нажмите любую клавишу . . .");
             Console.ReadKey();
         }
