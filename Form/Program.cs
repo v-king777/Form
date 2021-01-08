12a0ad3eb10ac2d2898aaae4443930576de4d2fb
v-kolp <v.kolpashchikov@gmail.com>
2021-01-08 21:38:33 +0300

    Добавьте файлы проекта.

---
+++ Form/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String MyName = "Vladimir";
+            byte MyAge = 37;
+            bool HavePet = false;
+            double MyShoeSize = 40.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("My age is " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HavePet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+            Console.ReadKey();
+        }
+    }
+}
